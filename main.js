fc5359c211b6c2e66a34a0d49916d5ca4eae7892
Tomek Gasowski <tomekgasowski93@gmail.com>
2021-11-16 21:03:49 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+alert('dupa');
+
+var firstName = "Tomek";
+
+
+const age = 28;
+
+console.log(firstName);
+
+console.log('elo mordo!');
+console.log(`Elo melo, jestem ${firstName} i mam ${age} lat`);
