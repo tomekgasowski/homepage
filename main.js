73bcd27869bc4e47777c0b3c18f10b1ee5a1cf5e
Tomek Gasowski <tomekgasowski93@gmail.com>
2021-11-17 17:28:04 +0100

    practising dom

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-
-alert('dupa');
-
 var firstName = "Tomek";
-
-
 const age = 28;
 
 console.log(firstName);
 
 console.log('elo mordo!');
 console.log(`Elo melo, jestem ${firstName} i mam ${age} lat`);
+
+const kombinacjeWszelakie = document.querySelector('.js-something');
+kombinacjeWszelakie.innerHTML = "niech bedzie dupa";
