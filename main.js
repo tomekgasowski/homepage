cfd80d65a057ca31743dd16f8b028a56311dc387
Tomek Gasowski <tomekgasowski93@gmail.com>
2022-01-22 13:40:10 +0100

    new colors

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ const button = document.querySelector('.hamburger--js');
 button.addEventListener('click', () => {
     const nav = document.querySelector('.navigation');
     nav.classList.toggle('navigation--open');
-})
+});
+
+
+
